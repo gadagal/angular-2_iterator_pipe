d62d37d2a1963e045759ffe7afaf81d1e8d29f44
gadagal <mehmetakifyener@gmail.com>
2017-03-20 14:14:11 +0300

    Create iterator pipe module

---
+++ iterator_pipe.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {IteratorPipe} from './iterator.pipe';
+
+@NgModule({
+    imports : [],
+    declarations : [IteratorPipe],
+    exports : [IteratorPipe]
+})
+export class IteratorModule{}
